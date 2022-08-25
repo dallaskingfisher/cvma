360fab358f211aff7c3ae36b348923b6ab7da687
dallaskingfisher <dallaskingfisher@icloud.com>
2022-08-25 09:51:38 -0500

    git update

--- cvma-webapp/test.js
+++
@@ -1,4 +0,0 @@
-const location =
-  "https://combatveteransmotorcycleassocaition.s3.us-east-1.amazonaws.com/next-s3-uploads/4f68bfe5-561d-4d0b-b0c0-a4e704eca979/ChapterMembers.xlsx";
-const key = location.slice(71);
-console.log(key);
